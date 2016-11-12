169c137f91be7bf9b9a0db7473a6537228d843c5
Ankit Kumar <ankitkumar.itbhu@gmail.com>
2016-11-13 01:40:20 +0530

    fixed definitions

--- TwistedFunctor.v
+++ TwistedFunctor.v
@@ -6,11 +6,11 @@ Open Scope program_scope.
 
 Class Monoid (A : Type) :=
   {
-    empty : A ;
+    one : A ;
     append : A -> A -> A ;
 
-    left_identity : forall x, append empty x = x ;
-    right_identity : forall x, append x empty = x ;
+    left_identity : forall x, append one x = x ;
+    right_identity : forall x, append x one = x ;
     associativity : forall x y z, append x (append y z) = append (append x y) z
   }.
 
@@ -18,27 +18,33 @@ Notation " m1 ♢ m2 " := (append m1 m2)
   (at level 40, left associativity).
 
 
-Class Commutative_Monoid (A : Type) (m : Monoid A) :=
+Class Commutative_Monoid (A : Type) :=
   {
+    underlying_monoid :> Monoid A;
+    
     commutativity : forall x y, x ♢ y = y ♢ x
   }.
 
+Print  Commutative_Monoid.
 
 Class Functor (f : Type -> Type) :=
   {
     fmap {a b : Type} : (a -> b) -> (f a) -> (f b);
 
-    homomorphism {a b c : Type} : forall (p : b -> c) (q : a -> b), fmap (p ∘ q) = (fmap p) ∘ (fmap q)
+    ftor_homomorphism {a b c : Type} : forall (p : b -> c) (q : a -> b), fmap (p ∘ q) = (fmap p) ∘ (fmap q)
   }.
 
-Class Applicative_Functor (f : Type -> Type) (f1 : Functor f) :=
+Check id.
+
+Class Applicative_Functor (f : Type -> Type) :=
   {
     pure {a : Type} : a -> f a;
-    app_star {a b : Type}: f (a -> b) -> f a -> f b;
+    app_star {a b : Type} : f (a -> b) -> f a -> f b;
 
-    identity : forall v, app_star (pure id) v = v;
-    composition : forall u v w, app_star (app_star (app_star (pure compose) u) v) w = app_star u (app_star v w); 
-    homomorphism : forall f x, app_star (pure f) (pure x) = pure (f x)
+    appftor_identity : forall {a : Type} (v : f a) , app_star (pure id) v = v;
+    appftor_composition : forall {a b c : Type} (u : f (b -> c)) (v : f (a -> b)) (w : f a), app_star (app_star (app_star (pure compose) u) v) w = app_star u (app_star v w); 
+    appftor_homomorphism : forall {a b: Type} (f : a -> b) (x : a), app_star (pure f) (pure x) = pure (f x)
   }.
 
 Notation " fab <*> fa " := (app_star fab fa) (at level 40, left associativity).
+
